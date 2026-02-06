b2ff2b708a5801ecc7f5e92d5d23778abad002d3
jimmydoh <jimmydoh@gmail.com>
2026-02-06 06:40:09 +0000

    Continue

--- src/utilities/context.py
+++ src/utilities/context.py
@@ -5,7 +5,7 @@ class HardwareContext:
     A class to hold references to hardware components such as audio, matrix, hid, and leds.
     This allows for easy access to these components throughout the application.
     """
-    def __init__(self, hid=None,audio=None, matrix=None, leds=None):
+    def __init__(self, hid=None, audio=None, matrix=None, leds=None):
         self.audio = audio
         self.matrix = matrix
         self.hid = hid
