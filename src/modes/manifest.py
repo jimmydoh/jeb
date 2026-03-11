57bf84197d6d405be3108f9245eaf361b81ad1ac
James Douglas <jimmydoh@gmail.com>
2026-03-11 13:55:12 +1000

    Add tutes to manifest

--- src/modes/manifest.py
+++ src/modes/manifest.py
@@ -416,6 +416,7 @@ MODE_REGISTRY |= {
         "class_name": "ConwaysLife",
         "icon": "CONWAYS_LIFE",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 510,
         "requires": ["CORE"],
         "settings": []
@@ -427,6 +428,7 @@ MODE_REGISTRY |= {
         "class_name": "LangtonsAnt",
         "icon": "LANGTONS_ANT",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 520,
         "requires": ["CORE"],
         "settings": [
@@ -445,6 +447,7 @@ MODE_REGISTRY |= {
         "class_name": "WolframAutomata",
         "icon": "WOLFRAM_AUTOMATA",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 530,
         "requires": ["CORE"],
         "settings": [
@@ -463,6 +466,7 @@ MODE_REGISTRY |= {
         "class_name": "LissajousMode",
         "icon": "LISSAJOUS",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 540,
         "requires": ["CORE"],
         "settings": []
@@ -474,6 +478,7 @@ MODE_REGISTRY |= {
         "class_name": "BoidsMode",
         "icon": "BOIDS",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 550,
         "requires": ["CORE"],
         "settings": []
@@ -485,6 +490,7 @@ MODE_REGISTRY |= {
         "class_name": "PlasmaMode",
         "icon": "PLASMA",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 560,
         "requires": ["CORE"],
         "settings": []
@@ -496,6 +502,7 @@ MODE_REGISTRY |= {
         "class_name": "FallingSandMode",
         "icon": "FALLING_SAND",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 570,
         "requires": ["CORE"],
         "settings": []
@@ -507,6 +514,7 @@ MODE_REGISTRY |= {
         "class_name": "BouncingSprite",
         "icon": "BOUNCING_SPRITE",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 580,
         "requires": ["CORE"],
         "settings": []
@@ -518,6 +526,7 @@ MODE_REGISTRY |= {
         "class_name": "Wireworld",
         "icon": "WIREWORLD",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 590,
         "requires": ["CORE"],
         "settings": []
@@ -529,6 +538,7 @@ MODE_REGISTRY |= {
         "class_name": "StarfieldMode",
         "icon": "STARFIELD",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 600,
         "requires": ["CORE"],
         "settings": [
@@ -547,6 +557,7 @@ MODE_REGISTRY |= {
         "class_name": "MechaForge",
         "icon": "MECHA_FORGE",
         "menu": "ZERO_PLAYER",
+        "has_tutorial": True,
         "order": 610,
         "requires": ["CORE"],
         "optional": ["INDUSTRIAL"],
